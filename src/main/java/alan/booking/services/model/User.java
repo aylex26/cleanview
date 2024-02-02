c85c8a35ef1a0e538e66a18f114944174242f55d
Alan Technologies <alantechnologies.ac@gmail.com>
2024-02-02 22:56:16 +0200

    .

--- src/main/java/alan/booking/services/model/User.java
+++ src/main/java/alan/booking/services/model/User.java
@@ -1,8 +1,11 @@
 package alan.booking.services.model;
 
+import jakarta.validation.constraints.Pattern;
+
 public record User(
         String name,
         String surname,
+        @Pattern(regexp = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$", message = "Adresa de email invalidă")
         String email,
         String password,
         long phoneNumber,
