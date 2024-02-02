b6c8e94e2ba879919476650cba3553003e18b5cd
Alan Technologies <alantechnologies.ac@gmail.com>
2024-02-02 23:03:43 +0200

    .

--- src/main/java/alan/booking/services/model/User.java
+++ src/main/java/alan/booking/services/model/User.java
@@ -4,13 +4,18 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
 public record User(
+        @NotBlank
         String name,
+        @NotBlank
         String surname,
         @NotBlank(message = "The email address cannot be empty")
         @Pattern(regexp = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$", message = "Invalid email address")
         String email,
+        @NotBlank
         String password,
+        @NotBlank
         long phoneNumber,
+        @NotBlank
         String address) {
 
 
