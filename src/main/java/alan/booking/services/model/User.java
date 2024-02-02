dba7ee4afa377672e4d7fbb0205c90094a3d08d4
Alan Technologies <alantechnologies.ac@gmail.com>
2024-02-02 22:42:19 +0200

    change the site

--- src/main/java/alan/booking/services/model/User.java
+++ src/main/java/alan/booking/services/model/User.java
@@ -1,4 +1,10 @@
 package alan.booking.services.model;
 
 public class User {
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private long phoneNumber;
+    private String address;
 }
