4c87ee1d635462fce60f5796da94e246db3b4b3c
Alan Technologies <alantechnologies.ac@gmail.com>
2024-02-02 22:52:26 +0200

    .

--- src/main/java/alan/booking/services/model/User.java
+++ src/main/java/alan/booking/services/model/User.java
@@ -1,6 +1,12 @@
 package alan.booking.services.model;
 
-public record User(String name, String surname, String email, String password, long phoneNumber, String address) {
+public record User(
+        String name,
+        String surname,
+        String email,
+        String password,
+        long phoneNumber,
+        String address) {
 
 
 }
