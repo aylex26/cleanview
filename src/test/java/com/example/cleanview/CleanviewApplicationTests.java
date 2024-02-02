e152e7361839215b49713bbbd2648868c19e837f
Alan Technologies <alantechnologies.ac@gmail.com>
2024-02-02 20:45:38 +0200

    added cleanview project

---
+++ src/test/java/com/example/cleanview/CleanviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cleanview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
